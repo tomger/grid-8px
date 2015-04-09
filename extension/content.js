429d0988acbab614512d37672bc91b30b9863b51
Tom Germeau <tom.germeau@gmail.com>
2015-04-09 15:48:08 -0400

    add chrome extension

---
+++ extension/content.js
@@ -0,0 +1,7 @@
+(function(d){
+  var path = "https://cdn.rawgit.com/tomger/grid-8px/master/grid-8px.js";
+  var e = d.createElement('script');
+  e.setAttribute('type','text/javascript');
+  e.setAttribute('src', path);
+  d.body.appendChild(e);
+})(document);
