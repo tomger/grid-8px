935fb1578c450b164dc6dab5b64db11ebf82fd34
Tom Germeau <tom.germeau@gmail.com>
2015-04-09 11:41:07 -0400

    Clean up code. Use readystatechange

--- grid-8px.js
+++ grid-8px.js
@@ -1,24 +1,92 @@
-// https://s3.amazonaws.com/tomgermeau.com/tools/grid-8px.js
+(function(){
+/**
+ * Shows a grid. Toggle with Ctrl+g or hold down Alt/Option key.
+ */
 
-var overlay = document.createElement('div');
-var svg = "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='8px' height='8px'><rect stroke-opacity='1' fill='none' stroke='#F7E1F6' x='0' y='0' width='9' height='9'></rect></svg>";
-var background = 'url("' + svg.replace(/8/g, '32').replace(/9/g, '33').replace('#F7E1F6', '#ADE2E0') + '"), url("' + svg + '")';
+var colorSmallBlock = '#F7E1F6';
+var colorBigBlock = '#ADE2E0';
+var sizeSmallBlock = 8;
+var sizeBigBlock = 32;
 
-var html = document.documentElement;
-var height = Math.max(document.body.scrollHeight, document.body.offsetHeight,
-                      html.clientHeight, html.scrollHeight, html.offsetHeight);
+runWhenComplete(initGrid);
 
-var style = 'position: absolute; opacity: 0; top: 0; left: 0; height: 100%; right: 0; z-index: 999; pointer-events: none; background: ' + background;
-overlay.setAttribute('style', style);
+function initGrid() {
+  console.info('running grid-8px 0.1 🐷🏁');
+  var height = findDocumentHeight();
+  var overlay = createOverlay(height);
+  var tempVisibility = false;
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.body.appendChild(overlay);
-  document.body.addEventListener('keydown', function(e) {
-    if(e.ctrlKey && e.which === 71 || e.altKey) {
-      overlay.style.opacity = 0.8;
+  function updateGridVisibility() {
+    overlay.style.opacity = localStorage.grid8px === 'on' || tempVisibility ? 0.8 : 0;
+  }
+
+  function onKeydown(e) {
+    if(e.ctrlKey && e.which === 71) {
+      localStorage.grid8px = localStorage.grid8px === 'on' ? 'off' : 'on';
+    } else if (e.altKey) {
+      tempVisibility = true;
     }
-  }, true);
-  document.body.addEventListener('keyup', function(e) {
-    overlay.style.opacity = 0;
-  }, true);
-}, true);
+    updateGridVisibility();
+  }
+
+  function onKeyup(e) {
+    if (e.which === 71) {
+      return;
+    }
+    tempVisibility = false;
+    updateGridVisibility();
+  }
+
+  document.body.appendChild(overlay);
+  document.body.addEventListener('keydown', onKeydown, true);
+  document.body.addEventListener('keyup', onKeyup , true);
+  updateGridVisibility();
+}
+
+/**
+ * Helper functions
+ */
+
+function createSvgBlock(size, color) {
+  var svgString = "data:image/svg+xml;utf8," +
+      "<svg xmlns='http://www.w3.org/2000/svg' width='" + size + "px' height='" + size + "px'>" +
+      "<rect stroke-opacity='1' fill='none' stroke='" + color + "' x='0' y='0' width='" + (size + 1) +
+      "' height='" + (size + 1) + "'>" +
+      "</rect></svg>";
+  return svgString;
+}
+
+function createOverlay(height) {
+  var div = document.createElement('div');
+  var background = 'url("' + createSvgBlock(sizeBigBlock, colorBigBlock) +'"),' +
+                   'url("' + createSvgBlock(sizeSmallBlock, colorSmallBlock ) + '")';
+  var style = 'position: absolute; opacity: 0; top: 0; left: 0; right: 0; z-index: 999; pointer-events: none; ' +
+      'background: ' + background + ';' +
+      'height:' + height + 'px;';
+  div.setAttribute('style', style);
+  return div;
+}
+
+function findDocumentHeight() {
+  var html = document.documentElement;
+  var height = Math.max(document.body.scrollHeight,
+                        document.body.offsetHeight,
+                        html.clientHeight,
+                        html.scrollHeight,
+                        html.offsetHeight);
+  return height;
+}
+
+function runWhenComplete(fn) {
+  if (document.readyState == 'complete') {
+    fn();
+  } else {
+    document.addEventListener('readystatechange', function() {
+      if (document.readyState == 'complete') {
+        fn();
+      }
+    }, true);
+  }
+}
+
+})();
